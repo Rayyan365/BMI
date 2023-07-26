367b3758878606d7b9d7d3b5b7b426a23ec00f49
Rayyan365 <rayanabdallah09@gamil.com>
2023-07-25 22:53:30 -0700

    BMI done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
-// Good Luck! You got this 💪🏾
+// Good Luck! You got this 
 // Write your code here.
+function BMI(weight, height) 
+{
+    const bmi = weight / (height * height);
+    return bmi
+  }
+const ali_weight = 73;
+const ali_Height = 1.77; 
+const ali_BMI = BMI(ali_weight, ali_Height);
+
+const mohamed_weight =80;
+const mohamed_Height = 1.85; 
+const mohamed_BMI = BMI(mohamed_weight, mohamed_Height);
+
+if (mohamed_BMI > ali_BMI) {
+  console.log("Mohamed has a bigger BMI");
+} else if (ali_BMI > mohamed_BMI) {
+  console.log("Ali has a bigger BMI");
+} else {
+  console.log("Mohamed and Ali have the same BMI");
+}
